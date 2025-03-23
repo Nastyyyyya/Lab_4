52796ca0e1ddcad142a157c4cb40e4ffc9059f7e
Anastasia Paslavska <a.paslavska2206@gmail.com>
2025-03-23 18:05:07 +0200

    add reports

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,14 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/test/js/**/*.js'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: './reports/junit',
+        outputName: 'junit.xml',
+      },
+    ],
+  ],
 };
